1d117894b9688d42aea4ced799b0df727410fbc9
Александър Димитров <aleks_d@gmx.de>
2008-08-25 00:43:51 +0200

    changed the model format slightly.
    Probably going to change a lot when I start doing the stuff on embedding.

--- src/intensionalExampleModels.pl
+++ src/intensionalExampleModels.pl
@@ -4,11 +4,13 @@
 /*========================================================================
    Example Models
 
-   model(Individuals,Worlds,AccessibleRelations).
+   model([Individuals],[Worlds],[AccessibleRelations]).
+   world([AcessibleBy],[Functions])
 ========================================================================*/
 
 example(1,model([d1,d2,d3,d4,d5]
-	, [ (w0,world(
+	, [ world( [curt]
+		,
 		[ f(0,mia,d1)
 		, f(0,vincent,d2)
 		, f(0,pumpkin,d3)
@@ -18,11 +20,7 @@ example(1,model([d1,d2,d3,d4,d5]
 		, f(1,customer,[d1,d2])
 		, f(1,robber,[d3,d4])
 		, f(2,love,[(d3,d4)])
-		]))
-	  , (w1,world(
-		[ f(1,snort,[d2])
-		, f(2,love,[(d2,d5)])
-		]))
+		])
 	  ]
 	, [(d1,w2)]
 	))
